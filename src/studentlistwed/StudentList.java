a0da742ec0a9c56488c1bc8fcefadfdaf53a67bf
Azad <133147390+singhazad97@users.noreply.github.com>
2023-05-19 11:31:40 -0400

    Update StudentList.java

--- src/studentlistwed/StudentList.java
+++ src/studentlistwed/StudentList.java
@@ -7,7 +7,7 @@ package studentlistwed;
 
 /**
  * Class that creates an Array of Students and prints each student's name.
- * @author dancye, 2023
+ * @author Azadveer, 2023
  */
 public class StudentList 
 {
